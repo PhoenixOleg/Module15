4eecc46349ebe03e94e16734d72f2703b8b4459a
PhoenixOleg <metall_2000@mail.ru>
2023-11-18 22:46:10 +0300

    Добавьте файлы проекта.

---
+++ Module15/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Module15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            // ???
+        }
+
+        public class Classroom
+        {
+            public List<string> Students = new List<string>();
+        }
+    }
+}
